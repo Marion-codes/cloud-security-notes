5fd935104f981152dd9548484a8bcc993eaf49f5
Marion <maconbrown1030@gmail.com>
2026-05-11 12:20:31 -0500

    Updated login system with account dictionary

--- login_system.py
+++ login_system.py
@@ -1,21 +1,37 @@
 print("Welcome to the Login System")
-correct_username = "admin"
-correct_password = "1234"
 
-attempts = 3
+users = {
+    "admin": "1234"
+}
 
-while attempts > 0:
+choice = input("Type login or create account: ")
+
+if choice == "create":
+    new_username = input("Create username: ")
+    
+    if new_username in users:
+        print("Username already exists")
+
+    else:
+        new_password = input("Create password: ")
+
+        users[new_username] = new_password
+
+        print("Account created successfully")
+    
+elif choice == "login":
     username = input("Enter username: ")
     password = input("Enter password: ")
 
-    if username == correct_username and password == correct_password:
-        print("Access granted")
-        break
+    if username in users and users[username] == password:
+        print("Login succcessful")
     else:
-        attempts -= 1
-        print("Access denied. Attemps left:", attempts)
+        print("Invalid login")
 
-if attempts == 0:
-        print("Account locked")
+elif choice == "exit":
+    print("Goodbye!")
+        
+else:
+    print("Invalid option")
           
                 
