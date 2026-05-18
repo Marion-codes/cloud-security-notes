772907235e54e7016e324c40b0d418585201ec74
Marion <maconbrown1030@gmail.com>
2026-05-18 11:37:39 -0500

    Added account lock system

--- login_system.py
+++ login_system.py
@@ -4,6 +4,10 @@ users = {
     "admin": "1234"
 }
 
+failed_attempts = {}
+
+locked_accounts = []
+
 while True:
 
     choice = input("Type login or create, or exit: ")
@@ -19,17 +23,40 @@ while True:
 
             users[new_username] = new_password
 
+            failed_attempts[new_username] = 0
+            
             print("Account created successfully")
     
     elif choice == "login":
+        
         username = input("Enter username: ")
         password = input("Enter password: ")
 
+        if username in locked_accounts:
+            print("This acccount is locked")
+
         if username in users and users[username] == password:
+            
             print("Login succcessful")
+
+            failed_attempts[username] = 0
+            
         else:
             print("Invalid login")
 
+            if username not in failed_attempts:
+                failed_attempts[username] = 0
+
+            failed_attempts[username] += 1
+
+            print("Attempts:", failed_attempts[username])
+
+            if failed_attempts[username] >= 3:
+
+                locked_accounts.append(username)
+
+                print("Account locked due to too many failed attempts")
+            
     elif choice == "exit":
         print("Goodbye!")
         break
