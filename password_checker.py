9183160f93b75678899e99db6e34edb6c428d0a6
Marion <maconbrown1030@gmail.com>
2026-05-19 12:05:37 -0500

    Fixed indentation, password checker fully working now

--- password_checker.py
+++ password_checker.py
@@ -1,17 +1,26 @@
-password = input("Enter a password: ")
+while True:
+    password = input("Enter a password (or type exit): ")
+    if password == "exit":
+        print("Goodbye!")
+        break
 
-has_upper = any(char.isupper() for char in password)
-has_lower = any(char.islower() for char in password)
-has_digit = any(char.isdigit() for char in password)
-has_special = any(not char.isalnum() for char in password)
+    has_upper = any(char.isupper() for char in password)
+    has_lower = any(char.islower() for char in password)
+    has_digit = any(char.isdigit() for char in password)
+    has_special = any(not char.isalnum() for char in password)
 
-if len(password) < 8:
-    print("Weak: Password should be at least 8 characters long")
-elif password.islower() or password.isupper():
-    print("Medium: Use both uppercase and lowercase letters")
-elif password.isalnum():
-    print("Medium: Add special characters (!, @, #, etc.)")
-else:
-     print("Your Password is Strong")
+    if len(password) < 8:
+        print("Weak: Password should be at least 8 characters long")
+    
+    elif not has_upper or not has_lower:
+        print("Medium: Use both uppercase and lowercase letters")
 
-input("Press Enter to exit!")
+    elif not has_digit or not has_lower:
+        print("Medium: Add numbers to your password")
+    
+    elif not has_special:
+        print("Medium: Add special characters (!, @, #, etc.)")
+    
+    else:
+        print("Your Password is Strong")
+
